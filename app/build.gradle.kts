fca548bbb04e42b6b52a2ce50b8aa7159bb30d2b
“shawn-mperfinan” <shawn.mperfinan@gmail.com>
2025-09-15 08:35:57 +0800

    Chore: build release and deployment setup
    - configure build flavors setup
    - configure firebase crashlytics setup
    - add necessary firebase and google dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,32 @@
+import com.google.firebase.crashlytics.buildtools.gradle.CrashlyticsExtension
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.devtools.ksp)
+    alias(libs.plugins.google.services)
+    alias(libs.plugins.firebase.crashlytics)
+}
+
+// Initialize for property retrieval on variables defined in [local.properties]
+val localProperties = Properties()
+localProperties.load(project.rootProject.file("local.properties").inputStream())
+
+// Calculates and get the versionCode based on commit counts
+fun getVersionCode(): Int {
+    // Run the Git command and get the commit count result as an integer
+    val commitCount =
+        providers.exec {
+            commandLine("git", "rev-list", "--no-merges", "--count", "HEAD")
+        }.standardOutput.asText.get().trim().toInt()
+
+    // kindly refer to the current value of versionName based on its MAJOR, MINOR and PATCH value then append 000
+    // current value --> versionName = 1.0.0
+    val semanticVersionCombination = 100000
+
+    return commitCount + semanticVersionCombination
 }
 
 android {
@@ -13,17 +37,10 @@ android {
         applicationId = "dev.mperfinan.pokedex"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
+        versionCode = getVersionCode()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
     }
 
     java {
@@ -36,6 +53,56 @@ android {
         compose = true
         buildConfig = true
     }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("../release/secret/android-keystore.jks")
+            keyAlias = localProperties.getProperty("ANDROID_KEY_ALIAS")
+            storePassword = localProperties.getProperty("ANDROID_KEYSTORE_PASSWORD")
+            keyPassword = localProperties.getProperty("ANDROID_KEY_PASSWORD")
+        }
+    }
+
+    flavorDimensions += "type"
+    productFlavors {
+        create("dev") {
+            dimension = "type"
+            applicationId = "dev.mperfinan.pokedex.dev"
+            versionNameSuffix = "-dev"
+
+            configure<CrashlyticsExtension> {
+                mappingFileUploadEnabled = false
+            }
+        }
+
+        create("live") {
+            dimension = "type"
+
+            configure<CrashlyticsExtension> {
+                mappingFileUploadEnabled = true
+            }
+        }
+    }
+
+    buildTypes {
+        getByName("debug") {
+            // Don’t upload mapping files for debug builds
+            configure<CrashlyticsExtension> {
+                mappingFileUploadEnabled = false
+            }
+        }
+
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            isShrinkResources = true
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
 }
 
 dependencies {
@@ -47,6 +114,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.crashlytics.ndk)
 
     testImplementation(libs.junit)
 
--- app/src/main/java/dev/mperfinan/pokedex/PokedexApplication.kt
+++ app/src/main/java/dev/mperfinan/pokedex/PokedexApplication.kt
@@ -1,5 +1,19 @@
 package dev.mperfinan.pokedex
 
 import android.app.Application
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 
-class PokedexApplication : Application()
+class PokedexApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val crashlytics = FirebaseCrashlytics.getInstance()
+
+        // Tag which flavor is running
+        crashlytics.setCustomKey("flavor", BuildConfig.FLAVOR)
+
+        // Optional: also tag version
+        crashlytics.setCustomKey("versionName", BuildConfig.VERSION_NAME)
+        crashlytics.setCustomKey("versionCode", BuildConfig.VERSION_CODE)
+    }
+}
