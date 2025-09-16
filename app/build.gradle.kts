8e115ee815ede69759ce7c54ee0693c7390a44fb
Shawn Perfiñan <shawn.mperfinan@gmail.com>
2025-09-16 11:40:32 +0800

    Chore: Build release deployment setup (#4)
    * Chore: build release and deployment setup
    
    - configure app launchers based on given product flavors
    - add necessary app launcher webp icons
    - configure build flavors setup
    - configure firebase crashlytics setup
    - add necessary firebase and google dependencies
    - configure fastlane setup for build releases
    - integrate slack hook notifying channel on release result status
    - integrate firebase app distribution on dev release build
    - setup pr review workflow to ensure overall code quality
    - setup build deployment workflow as its CI/CD automation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,40 @@
+import com.google.firebase.crashlytics.buildtools.gradle.CrashlyticsExtension
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.devtools.ksp)
+    alias(libs.plugins.firebase.crashlytics)
+    alias(libs.plugins.google.services)
+}
+
+// Load local.properties if it exists
+val localProperties =
+    File(rootDir, "local.properties").let { file ->
+        Properties().apply {
+            if (file.exists()) {
+                file.inputStream().use { load(it) }
+            } else {
+                println("⚠️ No local.properties found, using defaults")
+            }
+        }
+    }
+
+// Calculates and get the versionCode based on commit counts
+fun getVersionCode(): Int {
+    // Run the Git command and get the commit count result as an integer
+    val commitCount =
+        providers.exec {
+            commandLine("git", "rev-list", "--no-merges", "--count", "HEAD")
+        }.standardOutput.asText.get().trim().toInt()
+
+    // kindly refer to the current value of versionName based on its MAJOR, MINOR and PATCH value then append 000
+    // current value --> versionName = 1.0.0
+    val semanticVersionCombination = 100000
+
+    return commitCount + semanticVersionCombination
 }
 
 android {
@@ -13,17 +45,10 @@ android {
         applicationId = "dev.mperfinan.pokedex"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
+        versionCode = getVersionCode()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
     }
 
     java {
@@ -36,6 +61,65 @@ android {
         compose = true
         buildConfig = true
     }
+
+    signingConfigs {
+        create("release") {
+            val keyAlias = localProperties.getProperty("ANDROID_KEY_ALIAS")
+            val storePassword = localProperties.getProperty("ANDROID_KEYSTORE_PASSWORD")
+            val keyPassword = localProperties.getProperty("ANDROID_KEY_PASSWORD")
+            val storeFilePath = "../release/secret/android-keystore.jks"
+
+            if (keyAlias != null && storePassword != null && keyPassword != null) {
+                storeFile = file(storeFilePath)
+                this.keyAlias = keyAlias
+                this.storePassword = storePassword
+                this.keyPassword = keyPassword
+            } else {
+                println("⚠️ Skipping release signingConfig — no credentials found")
+            }
+        }
+    }
+
+    flavorDimensions += "type"
+    productFlavors {
+        create("dev") {
+            dimension = "type"
+            applicationId = "dev.mperfinan.pokedex.dev"
+            versionNameSuffix = "-dev"
+
+            configure<CrashlyticsExtension> {
+                mappingFileUploadEnabled = false
+            }
+        }
+
+        create("live") {
+            dimension = "type"
+
+            configure<CrashlyticsExtension> {
+                mappingFileUploadEnabled = true
+            }
+        }
+    }
+
+    buildTypes {
+        getByName("debug") {
+            // Don’t upload mapping files for debug builds
+            configure<CrashlyticsExtension> {
+                mappingFileUploadEnabled = false
+            }
+        }
+
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            isShrinkResources = true
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
 }
 
 dependencies {
@@ -47,6 +131,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.crashlytics.ndk)
 
     testImplementation(libs.junit)
 
--- app/src/main/java/dev/mperfinan/pokedex/PokedexApplication.kt
+++ app/src/main/java/dev/mperfinan/pokedex/PokedexApplication.kt
@@ -1,5 +1,24 @@
 package dev.mperfinan.pokedex
 
 import android.app.Application
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import dev.mperfinan.pokedex.util.Constants.APP_FLAVOR
+import dev.mperfinan.pokedex.util.Constants.APP_VERSION_CODE
+import dev.mperfinan.pokedex.util.Constants.APP_VERSION_NAME
 
-class PokedexApplication : Application()
+class PokedexApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        initializeFirebaseCrashlytics()
+    }
+
+    private fun initializeFirebaseCrashlytics() {
+        // Enable Firebase Crashlytics
+        FirebaseCrashlytics.getInstance().apply {
+            setCustomKey(APP_FLAVOR, BuildConfig.FLAVOR)
+            setCustomKey(APP_VERSION_NAME, BuildConfig.VERSION_NAME)
+            setCustomKey(APP_VERSION_CODE, BuildConfig.VERSION_CODE)
+        }
+    }
+}
