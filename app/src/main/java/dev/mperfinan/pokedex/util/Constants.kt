e52c73c2c5e39c1a68b3d28990f4233438e82a32
Shawn Perfiñan <shawn.mperfinan@gmail.com>
2025-09-17 10:16:44 +0800

    Initial project setup
    * chore: initial project setup with base configuration
    
    - create empty activity project
    - upgrade to latest Kotlin
    - set Java 21 compatibility
    - add independent AndroidApplication class
    - enable KSP support
    - update gradle.properties contents
    
    * Chore: code analyzer setup
    
    - configure code static analyzer via ktlint & detekt
    - apply code reformat on affected files
    
    * Chore: build release and deployment setup
    
    - configure app launchers based on given product flavors
    - add necessary app launcher webp icons
    - configure build flavors setup
    - configure firebase crashlytics setup
    - add necessary firebase and google dependencies
    - configure fastlane setup for build releases
    - integrate slack hook notifying channel on release result status
    - integrate firebase app distribution on dev release build
    - setup pr review workflow to ensure overall code quality
    - setup build deployment workflow as its CI/CD automation
    - add ruby and bundler setup
    
    * Docs: release and distribution
    
    - construct initial documentation of Release & Distribution Strategy section
    
    ---------
    
    Co-authored-by: “shawn-ffufm” <shawn.perfinan@ffufm.com>

---
+++ app/src/main/java/dev/mperfinan/pokedex/util/Constants.kt
@@ -0,0 +1,7 @@
+package dev.mperfinan.pokedex.util
+
+object Constants {
+    const val APP_FLAVOR = "flavor"
+    const val APP_VERSION_NAME = "versionName"
+    const val APP_VERSION_CODE = "versionCode"
+}
