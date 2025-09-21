b1f9fc5d61e9bc1d9b2397438f47e682f2ba62ba
“shawn-mperfinan” <shawn.mperfinan@gmail.com>
2025-09-21 12:53:38 +0800

    Feature: initial splash screen implementation
    - reassign launcher activity in AndroidManifest.xml file
    - add necessary assets for splash screen
    - implement custom display of splash screen

--- app/src/main/java/dev/mperfinan/pokedex/MainActivity.kt
+++ app/src/main/java/dev/mperfinan/pokedex/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dev.mperfinan.pokedex.ui.theme.PokedexandroidTheme
+import dev.mperfinan.pokedex.ui.theme.PokedexAndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PokedexandroidTheme {
+            PokedexAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -44,7 +44,7 @@ fun Greeting(
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    PokedexandroidTheme {
+    PokedexAndroidTheme {
         Greeting("Android")
     }
 }
---
+++ app/src/main/java/dev/mperfinan/pokedex/SplashActivity.kt
@@ -0,0 +1,127 @@
+package dev.mperfinan.pokedex
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.mperfinan.pokedex.ui.theme.PokedexAndroidTheme
+import kotlinx.coroutines.delay
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        setContent {
+            SplashScreen(
+                onTimeout =
+                    {
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    },
+            )
+        }
+    }
+
+    companion object {
+        const val INITIAL_BOUNCE_SCALE = 0.5f
+        const val INITIAL_ANIMATION_FADE = 0f
+        const val SPLASH_SCREEN_DURATION_DISPLAY = 2000L
+        const val TEXT_COLOR = 0xFFDF2C2C // TODO-LATER: this will be removed later once theming is finalized
+    }
+}
+
+@Composable
+private fun SplashScreen(onTimeout: () -> Unit) {
+    val scale = remember { Animatable(SplashActivity.INITIAL_BOUNCE_SCALE) } // Start smaller for bounce
+    val alpha = remember { Animatable(SplashActivity.INITIAL_ANIMATION_FADE) }
+
+    LaunchedEffect(Unit) {
+        // Bounce effect for the Pokemon ball
+        scale.animateTo(
+            targetValue = 1f,
+            animationSpec =
+                spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow,
+                ),
+        )
+
+        // Fade-in text after a short delay
+        alpha.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(durationMillis = 1000, delayMillis = 300),
+        )
+
+        // Keep splash visible for 2s
+        delay(timeMillis = SplashActivity.SPLASH_SCREEN_DURATION_DISPLAY)
+        onTimeout()
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_splash_pokemon_ball),
+                contentDescription = "Pokemon Ball",
+                modifier =
+                    Modifier
+                        .size(120.dp)
+                        // animated bounce
+                        .scale(scale.value),
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = stringResource(R.string.app_name),
+                fontSize = 32.sp,
+                fontFamily = FontFamily(Font(R.font.poppins_medium)),
+                color = Color(SplashActivity.TEXT_COLOR),
+                // animated fade
+                modifier = Modifier.alpha(alpha.value),
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    PokedexAndroidTheme {
+        SplashScreen {}
+    }
+}
